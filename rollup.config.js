3dd2684e4ae87a724a30e2f12e7f644ff8645149
Stanislav Lesnikov <stan@nashdot.com>
2016-02-23 21:36:49 +0100

    Use ternary converter to correct rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,16 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import npm from 'rollup-plugin-npm';
 import replace from 'rollup-plugin-replace';
+import ternary from 'rollup-plugin-ternary';
 
 export default {
     entry: 'src/index.js',
     dest: 'www/build/bundle.js',
     format: 'iife',
     plugins: [
+        ternary({
+            include: 'node_modules/react-router/**/*'
+        }),
         replace({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
